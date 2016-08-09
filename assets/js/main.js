a92332f2e718524d771e79f34a399cd92c800d38
Marius Kildedal <marius.kildedal@gmail.com>
2016-08-09 19:42:36 +0200

    la til rippel effekt på imdb knappen

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,34 @@
 
 //
 // };
+var isFF = !!navigator.userAgent.match(/firefox/i);
 
+Array.prototype.slice.call(document.querySelectorAll('.button'), 0).forEach(function(bt) {
+  bt.addEventListener('click', function(e) {
+    e.preventDefault();
+  });
+});
+
+
+initBt8();
+
+function initBt8() {
+  var bt = document.querySelectorAll('#component-8')[0];
+	var bt2 = document.querySelectorAll('#component-9')[0];
+  var turb = document.querySelectorAll('#filter-ripple-1 feImage')[0];
+  var dm = document.querySelectorAll('#filter-ripple-1 feDisplacementMap')[0];
+
+  bt.addEventListener('click', function(e) {
+    TweenLite.set(turb, { attr: { x: isFF ? e.offsetX : e.offsetX + 10, y: isFF ? e.offsetY : e.offsetY + 10, width: 0, height: 0 } });
+    TweenLite.to(turb, 3, { attr: { x: '-=300', y: '-=300', width: 600, height: 600 } });
+    TweenLite.fromTo(dm, 2, { attr: { scale: 30 } }, { attr: { scale: 0 } });
+  });
+	// bt2.addEventListener('click', function(e) {
+  //   TweenLite.set(turb, { attr: { x: isFF ? e.offsetX : e.offsetX + 10, y: isFF ? e.offsetY : e.offsetY + 10, width: 0, height: 0 } });
+  //   TweenLite.to(turb, 3, { attr: { x: '-=300', y: '-=300', width: 600, height: 600 } });
+  //   TweenLite.fromTo(dm, 2, { attr: { scale: 30 } }, { attr: { scale: 0 } });
+  // });
+}
 
 
 (function($) {
